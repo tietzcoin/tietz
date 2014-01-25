5812080968026ad8b640609afa108ebf31fd2a8c
Test <tietz@ubuntu.(none)>
2014-01-25 08:27:03 -0800

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("TIETZ");
+    case mBTC: return QString("mTIETZ");
+    case uBTC: return QString::fromUtf8("μTIETZ");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Tietzcoins");
+    case mBTC: return QString("Milli-Tietzcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Tietzcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
